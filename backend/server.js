5cd31e674723d7d00e54d86568d2f9201fd1bd3d
MakoDoli <makodolidze@hotmail.com>
2025-10-29 23:08:49 +0400

    initial server setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { config } from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Ready to serve");
+});
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server is listening on port whatever");
+});

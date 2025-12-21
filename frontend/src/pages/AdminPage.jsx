9862f4456929129d39b040dd6716b7d357e3d4b9
MakoDoli <makodolidze@hotmail.com>
2025-12-21 23:56:38 +0400

    add tabs to admin page

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
+const tabs = [
+  { id: "create", label: "Create Product", icon: PlusCircle },
+  { id: "products", label: "Products", icon: ShoppingBasket },
+  { id: "analytics", label: "Analytics", icon: BarChart },
+];
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  const [activeTab, setActiveTab] = useState("create");
+  return (
+    <div className="min-h-screen relative overflow-hidden bg-gray-900 text-white">
+      <div className="relative z-10 container mx-auto px-4 py-12"> </div>
+      <motion.h1
+        className="text-4xl font-bold mb-8 text-emerald-400"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`flex items-center px-4  py-2 mx-2 rounded-md transition-colors duration -200 ${
+              activeTab === tab.id
+                ? "bg-emerald-600 text-white"
+                : "bg-fray-700 text-gray-300"
+            }`}
+          >
+            <tab.icon className="mr-2 size-5" />
+            {tab.label}
+          </button>
+        ))}
+      </motion.h1>
+    </div>
+  );
 }
--- frontend/src/store/useUserStore.js
+++ frontend/src/store/useUserStore.js
@@ -38,7 +38,7 @@ export const useUserStore = create((set, get) => ({
         email,
         password,
       });
-      set({ user: res.data, loading: false });
+      set({ user: res.data.user, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.response.data.error || "An error occurred");

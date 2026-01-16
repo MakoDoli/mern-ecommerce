a77a82fc6e52aab7e56b204831731b4536bba2f6
MakoDoli <makodolidze@hotmail.com>
2026-01-16 20:34:06 +0400

    add product card

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,11 +1,44 @@
 import { useEffect } from "react";
 import { useProductStore } from "../store/useProductStore";
+import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+import ProductCard from "../components/ProductCard";
 
 export default function CategoryPage() {
   const { fetchProductsByCategory, products } = useProductStore();
+
+  const { category } = useParams();
   useEffect(() => {
-    fetchProductsByCategory("shoes");
-  }, [fetchProductsByCategory]);
+    fetchProductsByCategory(category);
+  }, [fetchProductsByCategory, category]);
   console.log(products);
-  return <div>CategoryPage</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="relative z-10 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 py-16 text-emerald-400 mb-8">
+        <motion.h1
+          className=""
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          {category.charAt(0).toUpperCase() + category.slice(1)}
+        </motion.h1>
+
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-center justify-items-center gap-8"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          {products.length === 0 ? (
+            <p>No products found in this category.</p>
+          ) : (
+            products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))
+          )}
+        </motion.div>
+      </div>
+    </div>
+  );
 }
